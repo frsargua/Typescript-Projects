112caf33edc985a80d5531462601373c8814dbc8
frsargua <frsargua@gmail.com>
2022-11-18 18:33:54 +0000

    used typescript to display json data

--- ProjectOne/src/index.tsx
+++ ProjectOne/src/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const App = () => {
-  (window as any).hello();
-  return <div>Hello world </div>;
-};
+import App from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
